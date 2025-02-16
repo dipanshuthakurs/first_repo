4e94c73bb8723d79b1976e40bc613c6a43e703dc
dipanshuthakurs <dipanshu.thakur@singulr.ai>
2025-02-16 11:52:39 +0530

    Add files via upload

---
+++ tryyyyyyyyyyyy.py
@@ -0,0 +1,30 @@
+userPrompt = input("Please enter")
+
+if userPrompt == "help":
+    print("This tool is useful for managing your AWS resources.")
+
+
+def sumTwoNumbers(a, b):
+    return a + b
+
+
+def subtractTwoNumbers(a, b):
+    return a - b
+
+firstNumber = 3
+secondNumber = 2
+
+print(f"The sum of {firstNumber} and {secondNumber} is: {sumTwoNumbers(firstNumber, secondNumber)}")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"The difference of {a} and {b} is: {subtractTwoNumbers(a, b)}")
+
+matrix = [[1,2],[3,4]]
+print(matrix)
+matrix2 = [[5,6],[7,8]]
+print(matrix2)
+
+result =matrix @ matrix2
+print(result)

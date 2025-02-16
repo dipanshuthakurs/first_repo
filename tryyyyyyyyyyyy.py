c6264495da011a14c55fd2d4b2a9a3e669234ad2
dipanshuthakurs <dipanshu.thakur@singulr.ai>
2025-02-16 12:30:02 +0530

    Update tryyyyyyyyyyyy.py

--- tryyyyyyyyyyyy.py
+++ tryyyyyyyyyyyy.py
@@ -1,8 +1,4 @@
-userPrompt = input("Please enter")
-
-if userPrompt == "help":
-    print("This tool is useful for managing your AWS resources.")
-
+userPrompt = input("Please enter the input")
 
 def sumTwoNumbers(a, b):
     return a + b
@@ -16,10 +12,6 @@ secondNumber = 2
 
 print(f"The sum of {firstNumber} and {secondNumber} is: {sumTwoNumbers(firstNumber, secondNumber)}")
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-print(f"The difference of {a} and {b} is: {subtractTwoNumbers(a, b)}")
 
 matrix = [[1,2],[3,4]]
 print(matrix)
